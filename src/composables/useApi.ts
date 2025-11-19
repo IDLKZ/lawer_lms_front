738f8fdc948ea9e19c041d45b3b48aa856ef168c
nurba <idlkz2019@gmail.com>
2025-11-19 13:16:43 +0500

    commit fixed

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -4,7 +4,11 @@ import router from '@/router'
 
 // Создаем axios instance
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json'
+    }
 })
 
 // Request Interceptor: добавление JWT токена
